d0146eff4b2baa487355022eed0dd29a6eedb059
Herman Sletmoen <hermansletmoen@gmail.com>
2023-03-02 21:09:22 +0100

    Clean up and comment background cosmology code

--- Cosmology.jl
+++ Cosmology.jl
@@ -5,127 +5,132 @@ export ΛCDM
 export H, aH, daH, d2aH
 export t, η, Ωγ, Ων, Ωb, Ωc, Ωk, ΩΛ, Ωr, Ωm, Ω
 export r_m_equality, m_Λ_equality, acceleration_onset
-export dL
+export dL, dA
 
 include("Constants.jl")
 
-using DifferentialEquations
+using DifferentialEquations # ODE integration
 using Dierckx # spline interpolation
-using Roots
+using Roots # root finding
 using .Constants
 
 mutable struct ΛCDM
-    const h::Float64 # dimensionless Hubble parameter h = H0 / (100 TODO units)
-    const H0::Float64
+    const h0::Float64  # dimensionless Hubble parameter h0 = H0 / (100 (km/s/Mpc))
+    const H0::Float64  # dimensionful  Hubble parameter
     const Ωb0::Float64 # baryons
     const Ωc0::Float64 # cold dark matter
     const Ωm0::Float64 # matter = baryons + cold dark matter
     const Ωk0::Float64 # curvature
     const Ωγ0::Float64 # photons
-    const Ων0::Float64 # (massless) neutrinos
+    const Ων0::Float64 # neutrinos (massless)
     const Ωr0::Float64 # radiation = photons + neutrinos
-    const ΩΛ::Float64 # dark energy (as cosmological constant)
+    const ΩΛ0::Float64 # dark energy (as cosmological constant)
     const Tγ0::Float64 # photon (CMB) temperature
-    const Neff::Float64 # TODO: ?
+    const Neff::Float64 # effective neutrino number
 
-    η_spline::Union{Nothing, Dierckx.Spline1D} # TODO: what kind of spline?
-    t_spline::Union{Nothing, Dierckx.Spline1D}
+    η_spline::Union{Nothing, Dierckx.Spline1D} # conformal time spline (lazily initialized)
+    t_spline::Union{Nothing, Dierckx.Spline1D} # cosmic    time spline (lazily initialized)
 
     function ΛCDM(; h=0.67, Ωb0=0.05, Ωc0=0.267, Ωk0=0, Tγ0=2.7255, Neff=3.046)
-        H0 = h * 100*km/Mpc
+        H0  = h * 100*km/Mpc # 1/s
         Ωm0 = Ωb0 + Ωc0
-        Ωγ0 = 2 * π^2/30 * (kB*Tγ0)^4 / (ħ^3*c^5) * 8*π*G / (3*H0^2)
+        Ωγ0 = π^2/15 * (kB*Tγ0)^4 / (ħ^3*c^5) * 8*π*G / (3*H0^2)
         Ων0 = Neff * 7/8 * (4/11)^(4/3) * Ωγ0
         Ωr0 = Ωγ0 + Ων0
-        ΩΛ  = 1.0 - (Ωr0 + Ωm0 + Ωk0)
-        Ω0 = Ωr0 + Ωm0 + Ωk0 + ΩΛ # ≈ 1
-        new(h, H0, Ωb0, Ωc0, Ωm0, Ωk0, Ωγ0, Ων0, Ωr0, ΩΛ, Tγ0, Neff, nothing, nothing)
+        ΩΛ0 = 1.0 - (Ωr0 + Ωm0 + Ωk0)
+        new(h, H0, Ωb0, Ωc0, Ωm0, Ωk0, Ωγ0, Ων0, Ωr0, ΩΛ0, Tγ0, Neff, nothing, nothing)
     end
 end
 
-Base.broadcastable(co::ΛCDM) = Ref(co) # never broadcast cosmology (in vectorized calls)
+# in vectorized calls, like H.(co, [1.0, 2.0]),
+# broadcast the same cosmology to all scalar calls
+Base.broadcastable(co::ΛCDM) = Ref(co)
 
-# internal time variable: natural logarithm of scale factor
-x(a::Real) = log(a)
-a(x::Real) = exp(x)
-z(x::Real) = 1/a(x) - 1
+# convert between (a, x, z) across a = e^x = 1/(z+1)
+x(a::Real) = log(a)     # natural logarithm of scale factor (our internal time variable)
+a(x::Real) = exp(x)     # scale factor
+z(x::Real) = 1/a(x) - 1 # redshift
+
+# the d'th derivative of the thing in the square root in the Friedmann equation, E = H^2 / H0^2
+E(co::ΛCDM, x::Real; d::Integer=0) = (-4)^d * co.Ωr0/a(x)^4 +
+                                     (-3)^d * co.Ωm0/a(x)^3 +
+                                     (-2)^d * co.Ωk0/a(x)^2 +
+                                        0^d * co.ΩΛ0          # 0^0 = 1, 0^1 = 0, 0^2 = 0, ...
 
 # Friedmann equation
-# TODO: rename E
-Ωpoly(Ωr0::Real, Ωm0::Real, Ωk0::Real, ΩΛ::Real, x::Real; d::Integer=0) = (-4)^d * Ωr0/a(x)^4 + (-3)^d * Ωm0/a(x)^3 + (-2)^d * Ωk0/a(x)^2 + 0^d * ΩΛ # evolution of densities relative to *today's* critical density # TODO: can do derivative here (0^0 = 1 in Julia)! TODO: call it Ωpoly?
-Ωpoly(co::ΛCDM, x::Real; d::Integer=0) = Ωpoly(co.Ωr0, co.Ωm0, co.Ωk0, co.ΩΛ, x; d)
-H(h::Real, Ωr0::Real, Ωm0::Real, Ωk0::Real, ΩΛ::Real, x::Real) = h * 100 * km/Mpc * √(Ωpoly(Ωr0, Ωm0, Ωk0, ΩΛ, x))
-H(co::ΛCDM, x::Real) = H(co.h, co.Ωγ0+co.Ων0, co.Ωb0+co.Ωc0, co.Ωk0, co.ΩΛ, x) # "normal" Hubble parameter
+   H(co::ΛCDM, x::Real) = co.H0 * √(E(co, x)) # cosmic    Hubble parameter
+  aH(co::ΛCDM, x::Real) = a(x) * H(co, x)     # conformal Hubble parameter
+ daH(co::ΛCDM, x::Real) = aH(co, x) * (1 + 1/2 * E(co, x; d=1) / E(co, x)) # 1st derivative of aH
+d2aH(co::ΛCDM, x::Real) = aH(co, x) * (1 + E(co, x; d=1) / E(co, x) + 1/2 * E(co, x; d=2) / E(co, x) - 1/4 * (E(co, x; d=1) / E(co, x))^2) # 2nd derivative of aH
 
-# conformal Hubble parameter (𝓗 = a*H) + 1st derivative + 2nd derivative (from analytical considerations)
-  aH(co::ΛCDM, x::Real) = a(x) * H(co, x)
- daH(co::ΛCDM, x::Real) = aH(co, x) * (1 + 1/2 * Ωpoly(co, x; d=1) / Ωpoly(co, x))
-d2aH(co::ΛCDM, x::Real) = aH(co, x) * (1 + Ωpoly(co, x; d=1) / Ωpoly(co, x) + 1/2 * Ωpoly(co, x; d=2) / Ωpoly(co, x) - 1/4 * (Ωpoly(co, x; d=1) / Ωpoly(co, x))^2)
-
+# internal function that computes y(x) on a cubic spline,
+# given dy/dx, from x=x1 with y(x1)=y1, to x=x2
 function _spline_dy_dx(co::ΛCDM, dy_dx::Function, x1::Float64, x2::Float64, y1::Float64)
     sol = solve(ODEProblem((y, p, x) -> dy_dx(x), y1, (x1, x2)), Tsit5(); reltol=1e-10)
     xs, ys = sol.t, sol.u
-    return Spline1D(xs, ys; k=3, bc="error"), x1, x2
+    return Spline1D(xs, ys; k=3, bc="error") # throw error if evaluating spline outside bounds
 end
 
 # conformal time
 function η(co::ΛCDM, x::Real)
     if isnothing(co.η_spline)
-        dη_dx(x) = 1 / (a(x) * H(co, x))
+        # lazy initialize spline
+        dη_dx(x) = 1 / aH(co, x)
         x1, x2 = -20.0, +20.0
         aeq = co.Ωr0 / co.Ωm0
         if co.Ωm0 > 0
-            η1 = 2 / (co.H0*√(co.Ωm0)) * (√(a(x1)+aeq) - √(aeq))
+            η1 = 2 / (co.H0*√(co.Ωm0)) * (√(a(x1)+aeq) - √(aeq)) # anal expr with Ωk=ΩΛ=0
         else
-            η1 = 1 / aH(co, x1)
+            η1 = 1 / aH(co, x1) # anal expr with Ωm=Ωk=ΩΛ=0
         end
-        co.η_spline, x1, x2 = _spline_dy_dx(co, dη_dx, x1, x2, η1)
+        co.η_spline = _spline_dy_dx(co, dη_dx, x1, x2, η1)
     end
-    (x1, x2) = extrema(get_knots(co.η_spline))
-    return x1 <= x <= x2 ? co.η_spline(x) : NaN
+    return co.η_spline(x)
 end
 
 # cosmic time
 function t(co::ΛCDM, x::Real)
     if isnothing(co.t_spline)
+        # lazy initialize spline
         dt_dx(x) = 1 / H(co, x)
-        x1, x2 = -20.0, +20.0 # integration and spline range
+        x1, x2 = -20.0, +20.0
         aeq = co.Ωr0 / co.Ωm0
         if co.Ωm0 > 0
-            t1 = 2 / (3*co.H0*√(co.Ωm0)) * (√(a(x1)+aeq) * (a(x1)-2*aeq) + 2*aeq^(3/2))
+            t1 = 2 / (3*co.H0*√(co.Ωm0)) * (√(a(x1)+aeq) * (a(x1)-2*aeq) + 2*aeq^(3/2)) # anal expr with Ωk=ΩΛ=0
         else
-            t1 = 1 / (2*H(co, x1))
+            t1 = 1 / (2*H(co, x1)) # anal expr with Ωm=Ωk=ΩΛ=0
         end
-        co.t_spline, x1, x2 = _spline_dy_dx(co, dt_dx, x1, x2, t1)
+        co.t_spline = _spline_dy_dx(co, dt_dx, x1, x2, t1)
     end
-    (x1, x2) = extrema(get_knots(co.t_spline))
-    return x1 <= x <= x2 ? co.t_spline(x) : NaN
+    return co.t_spline(x)
 end
 
 # density parameters (relative to critical density *at the time*)
-# computed using Ωs = ρs/ρcrit = ρs/ρcrit0 * ρcrit0/ρcrit = Ωs0 * H0^2/H^2
-Ωγ(co::ΛCDM, x::Real) = co.Ωγ0 / (a(x)^4 * H(co, x)^2 / H(co, 0)^2)
-Ων(co::ΛCDM, x::Real) = co.Ων0 / (a(x)^4 * H(co, x)^2 / H(co, 0)^2)
-Ωb(co::ΛCDM, x::Real) = co.Ωb0 / (a(x)^3 * H(co, x)^2 / H(co, 0)^2)
-Ωc(co::ΛCDM, x::Real) = co.Ωc0 / (a(x)^3 * H(co, x)^2 / H(co, 0)^2)
-Ωk(co::ΛCDM, x::Real) = co.Ωk0 / (a(x)^2 * H(co, x)^2 / H(co, 0)^2)
-ΩΛ(co::ΛCDM, x::Real) = co.ΩΛ  / (H(co, x)^2 / H(co, 0)^2)
+# computed using Ωs = ρs/ρcrit = ρs/ρcrit0 * ρcrit0/ρcrit = Ωs0 * H0^2/H^2 = Ωs0 / E^2
+Ωγ(co::ΛCDM, x::Real) = co.Ωγ0 / a(x)^4 / E(co, x)
+Ων(co::ΛCDM, x::Real) = co.Ων0 / a(x)^4 / E(co, x)
+Ωb(co::ΛCDM, x::Real) = co.Ωb0 / a(x)^3 / E(co, x)
+Ωc(co::ΛCDM, x::Real) = co.Ωc0 / a(x)^3 / E(co, x)
+Ωk(co::ΛCDM, x::Real) = co.Ωk0 / a(x)^2 / E(co, x)
+ΩΛ(co::ΛCDM, x::Real) = co.ΩΛ0          / E(co, x)
 Ωr(co::ΛCDM, x::Real) = Ωγ(co, x) + Ων(co, x)
 Ωm(co::ΛCDM, x::Real) = Ωb(co, x) + Ωc(co, x)
 Ω( co::ΛCDM, x::Real) = Ωr(co, x) + Ωm(co, x) + Ωk(co, x) + ΩΛ(co, x)
 
 # equalities
 r_m_equality(co::ΛCDM) = log(co.Ωr0 / co.Ωm0)
-m_Λ_equality(co::ΛCDM) = log(co.Ωm0 / co.ΩΛ) / 3
+m_Λ_equality(co::ΛCDM) = log(co.Ωm0 / co.ΩΛ0) / 3
 acceleration_onset(co::ΛCDM) = find_zero(x -> daH(co, x), (-20, +20))
 
 # luminosity distance
 function dL(co::ΛCDM, x::Real)
     χ = c * (η(co, 0) - η(co, x))
+
+    # ALL three expressions
     # 1. r(Ωk0 = 0) = χ
     # 2. r(Ωk0 < 0) = χ *  sin(√(-Ωk0)*H0*χ/c) / (√(-Ωk0)*H0*χ/c)
     # 3. r(Ωk0 > 0) = χ * sinh(√(+Ωk0)*H0*χ/c) / (√(+Ωk0)*H0*χ/c)
-    # can all be written as the real-valued function
+    # can be written as the real-valued function
     #    r(Ωk0)     = χ * sinc(√(-Ωk0)*H0*χ/c/π)
     # using complex numbers,
     # because sinc(x) = sin(π*x) / (π*x) -> 1 as x -> 0,
@@ -138,27 +143,31 @@ function dA(co::ΛCDM, x::Real)
     return dL(co, x) * a(x)^2
 end
 
-# EXAMPLES:
-# Cosmology.ΛCDM(Ωr0=0, Ωb0=0, Ωc0=0.2, Ωk0=-0.9) has Ωpoly(x ≈ -1) < 0
+# checks whether the Hubble parameter becomes complex on the integration interval (x1, x2)
+# EXAMPLE: Cosmology.ΛCDM(Ωr0=0, Ωb0=0, Ωc0=0.2, Ωk0=-0.9) has E(x ≈ -1) < 0
 function is_fucked(co::ΛCDM; x1=-20.0, x2=+20.0)
-    if Ωpoly(co, x1) < 0 || Ωpoly(co, x2) < 0
+    # negative at the endpoints?
+    if E(co, x1) < 0 || E(co, x2) < 0
         return true
     end
 
-    a = -4 * co.Ωr0
-    b = -3 * co.Ωm0
-    c = -2 * co.Ωk0
+    # negative between the endpoints?
+    # check the values at the stationary points of E(x), defined by roots of 2nd degree polynomial
+    # dE/dx = -a^(-2) * [ 4*Ωr0*a^(-2) + 3*Ωm0*a^(-1) + 2*Ωk0 ] = 0
+    a = 4 * co.Ωr0
+    b = 3 * co.Ωm0
+    c = 2 * co.Ωk0
     d = b^2 - 4*a*c
     if d >= 0
         ainv1 = (-b + √(d)) / (2*a)
         ainv2 = (-b - √(d)) / (2*a)
         a1, a2 = 1/ainv1, 1/ainv2
-        if (a1 >= 0 && Ωpoly(co, x(a1)) < 0) || (a2 >= 0 && Ωpoly(co, x(a2)) < 0)
+        if (a1 >= 0 && E(co, x(a1)) < 0) || (a2 >= 0 && E(co, x(a2)) < 0)
             return true
         end
     end
 
-    return false
+    return false # always positive
 end
 
 end
--- Milestone1.jl
+++ Milestone1.jl
@@ -70,19 +70,19 @@ if !isdir("plots")
 end
 
 # Conformal Hubble parameter
-if !isfile("plots/conformal_Hubble.pdf")
+if true || !isfile("plots/conformal_Hubble.pdf")
     println("Plotting conformal Hubble parameter")
     plot(xlabel = L"x = \log a", ylabel = L"\log_{10}\Big[ \mathcal{H} \,/\, (100\,\mathrm{km/s/Mpc})\Big]", legend_position = :topleft, ylims = (-1, +7), yticks = -1:1:+7)
     plot!(x, @. log10(co.H0     / (100*km/Mpc) * √(co.Ωr0) * a(x)^(-1  )); linestyle = :dash,  label = "radiation-dominated")
     plot!(x, @. log10(co.H0     / (100*km/Mpc) * √(co.Ωm0) * a(x)^(-1/2)); linestyle = :dash,  label = "matter-dominated")
-    plot!(x, @. log10(co.H0     / (100*km/Mpc) * √(co.ΩΛ)  * a(x)^(+1  )); linestyle = :dash,  label = "cosmological constant-dominated")
+    plot!(x, @. log10(co.H0     / (100*km/Mpc) * √(co.ΩΛ0) * a(x)^(+1  )); linestyle = :dash,  label = "cosmological constant-dominated")
     plot!(x, @. log10(aH(co, x) / (100*km/Mpc)                          ); linestyle = :solid, label = "general case", color = :black)
     vline!([xrm, xmΛ], z_order = :back, color = :gray, linestyle = :dash, label = nothing)
     savefig("plots/conformal_Hubble.pdf")
 end
 
 # conformal Hubble parameter 1st derivative
-if !isfile("plots/conformal_Hubble_derivative1.pdf")
+if true || !isfile("plots/conformal_Hubble_derivative1.pdf")
     println("Plotting conformal Hubble 1st derivative")
     plot(xlabel = L"x = \log a", ylabel = L"\frac{1}{\mathcal{H}} \frac{\mathrm{d}\mathcal{H}}{\mathrm{d} x}", legend_position = :topleft)
     plot!(x, x -> -1;                   linestyle = :dash,  label = "radiation-dominated")
@@ -94,7 +94,7 @@ if !isfile("plots/conformal_Hubble_derivative1.pdf")
 end
 
 # Conformal Hubble parameter 2nd derivative
-if !isfile("plots/conformal_Hubble_derivative2.pdf")
+if true || !isfile("plots/conformal_Hubble_derivative2.pdf")
     println("Plotting conformal Hubble 2nd derivative")
     plot(xlabel = L"x = \log a", ylabel = L"\frac{1}{\mathcal{H}} \frac{\mathrm{d}^2\mathcal{H}}{\mathrm{d} x^2}", legend_positions = :topleft, yticks = 0:0.25:1.5, ylims = (0, 1.5))
     plot!(x, x -> ( 1)^2;                linestyle = :dash,  label = "radiation-dominated")
@@ -125,7 +125,7 @@ if true || !isfile("plots/eta_H.pdf")
 end
 
 # Cosmic and conformal time
-if !isfile("plots/times.pdf")
+if true || !isfile("plots/times.pdf")
     println("Plotting cosmic and conformal times")
     plot(xlabel = L"x = \log a", ylabel = L"\log_{10} \Big[ \{t, \eta\} / \mathrm{Gyr} \Big]", legend_position = :bottomright)
 
@@ -146,7 +146,7 @@ if !isfile("plots/times.pdf")
 end
 
 # Density parameters
-if !isfile("plots/density_parameters.pdf")
+if true || !isfile("plots/density_parameters.pdf")
     println("Plotting density parameters")
     plot(xlabel = L"x = \log a", ylabel = L"\Omega_i", legend_position = (0.05, 0.6), ylims=(-0.05, +1.3))
     plot!(x, Ωr.(co, x); label = L"\Omega_r")
@@ -183,7 +183,7 @@ end
 
 # Supernova MCMC fits
 # Inspiration: "A theoretician's analysis of the supernova data ..." (https://arxiv.org/abs/astro-ph/0212573)
-if !isfile("plots/supernova_omegas.pdf") || !isfile("plots/supernova_hubble.pdf")
+if true || !isfile("plots/supernova_omegas.pdf") || !isfile("plots/supernova_hubble.pdf")
     println("Plotting Ωm0, ΩΛ from MCMC analysis of supernova data")
 
     data = readdlm("data/supernovadata.txt", comments=true)
@@ -212,7 +212,7 @@ if !isfile("plots/supernova_omegas.pdf") || !isfile("plots/supernova_hubble.pdf"
     h, Ωm0, Ωk0, χ2 = params[:,1], params[:,2], params[:,3], -2 * logL
 
     # compute corresponding ΩΛ values by reconstructing the cosmologies (this is computationally cheap)
-    ΩΛ0 = [ΛCDM(h=h[i], Ωb0=0.05, Ωc0=Ωm0[i]-0.05, Ωk0=Ωk0[i], Neff=0).ΩΛ for i in 1:length(h)] # "ΩΛ0" to distinguish from Cosmology.ΩΛ
+    ΩΛ0 = [ΛCDM(h=h[i], Ωb0=0.05, Ωc0=Ωm0[i]-0.05, Ωk0=Ωk0[i], Neff=0).ΩΛ0 for i in 1:length(h)]
     ΩΛ0bounds = (0.0, 1.2) # just for later plotting
 
     # best fit
@@ -240,7 +240,7 @@ if !isfile("plots/supernova_omegas.pdf") || !isfile("plots/supernova_hubble.pdf"
 
     # plot ΩΛ(Ωm0) for a few flat universes (should give ΩΛ ≈ 1 - Ωm0)
     Ωm0_flat = range(Ωm0bounds..., length=20)
-    ΩΛ0_flat = [ΛCDM(h=best_h, Ωb0=0.05, Ωc0=Ωm0-0.05, Ωk0=0, Neff=0).ΩΛ for Ωm0 in Ωm0_flat]
+    ΩΛ0_flat = [ΛCDM(h=best_h, Ωb0=0.05, Ωc0=Ωm0-0.05, Ωk0=0, Neff=0).ΩΛ0 for Ωm0 in Ωm0_flat]
     plot!(Ωm0_flat, ΩΛ0_flat; color = :black, marker = :circle, markersize = 2, label = "flat universes")
 
     scatter!([best_Ωm0], [best_ΩΛ0]; color = :red, markerstrokecolor = :red, markershape = :cross, markersize = 10, label = "our best fit")
