bd0129c356a730bc73980082e822144acfd536e5
Herman Sletmoen <hermansletmoen@gmail.com>
2023-04-17 14:25:36 +0200

    Plot more Θl

--- Milestone3.jl
+++ Milestone3.jl
@@ -8,7 +8,7 @@ using .Cosmology
 using .Constants
 using LaTeXStrings
 
-co = ΛCDM(Neff=0)
+co = ΛCDM(h=0.7, Neff=0, Ωb0=0.05, Ωc0=0.45, Yp=0, z_reion_H=NaN) # TODO: change back
 k = 0.15 / Mpc
 xtce = time_tight_coupling(co, k)
 println("Tight coupling end (k = $(k*Mpc)/Mpc): ", format_time_variations(co, xtce))
@@ -25,13 +25,24 @@ end
 
 if true || !isfile("plots/overdensity.pdf") || !isfile("plots/velocity.pdf") || !isfile("plots/potential.pdf") || !isfile("plots/temperature_fluctuation.pdf")
     k = 0.01 / Mpc
-    x = range(-18, 0, length=200)
+    x = range(-18, 0, length=5000)
     println("Using k = $k / Mpc")
+
+    println("Plotting temperature fluctuation")
+    plot(xlabel=L"x = \log a", ylabel=L"\Theta")
+    # TODO: wrong sign with l=1?
+    for l in 0:3
+        plot!(x, Θl.(co, x, k, l), label=L"\Theta_%$(l)")
+    end
+    vline!([time_tight_coupling(co, k)], color=:gray, linestyle=:dash)
+    savefig("plots/temperature_fluctuation.pdf")
+
+    # return
 
     println("Plotting overdensity")
     plot(xlabel=L"x = \log a", ylabel=L"\log_{10} \delta")
     plot!(x, log10.(δc.(co, x, k)))
-    plot!(x, log10.(δb.(co, x, k)))
+    plot!(x, log10.(abs.(δb.(co, x, k))))
     savefig("plots/overdensity.pdf")
 
     println("Plotting velocity")
@@ -45,12 +56,6 @@ if true || !isfile("plots/overdensity.pdf") || !isfile("plots/velocity.pdf") || 
     plot!(x, Φ.(co, x, k))
     plot!(x, Φ.(co, x, k))
     savefig("plots/potential.pdf")
-
-    println("Plotting temperature fluctuation")
-    plot(xlabel=L"x = \log a", ylabel=L"\Theta")
-    plot!(x, Θl.(co, x, k, 0))
-    plot!(x, Θl.(co, x, k, 1)) # TODO: wrong sign?
-    savefig("plots/temperature_fluctuation.pdf")
 end
 
 end
