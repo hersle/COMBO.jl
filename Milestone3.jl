d3c499ec0c8808fbad19c67276bd023ec0dbb3bb
Herman Sletmoen <hermansletmoen@gmail.com>
2023-04-26 17:23:00 +0200

    Give perturbation plotter access to all perturbations; plot Φ+Ψ

--- Milestone3.jl
+++ Milestone3.jl
@@ -24,21 +24,25 @@ function extendx(x::Vector{Float64}, nextra::Integer)
 end
 
 if true || !isfile("plots/overdensity.pdf") || !isfile("plots/velocity.pdf") || !isfile("plots/potential.pdf") || !isfile("plots/temperature_fluctuation.pdf") # TODO: add more temperature fluctuations...
-    settings = [
-        ("plots/potential.pdf", L"\{\Phi,\Psi\}", [(Cosmology.i_Φ, identity, :solid, L"\Phi"), (Cosmology.i_Ψ, identity, :dash, L"\Psi")]),
-        ("plots/velocity.pdf",  L"\log_{10}|v|", [(Cosmology.i_vc, log10, :solid, L"v=v_c"), (Cosmology.i_vb, log10∘abs, :dash, L"v=v_b")]),
-        ("plots/velocity2.pdf", L"\log_{10}|v|", [(Cosmology.i_Θl(1), Θ1 -> log10(abs(-3*Θ1)), :solid, L"v=v_\gamma=-3\Theta_1"), (Cosmology.i_Nl(1), N1 -> log10(abs(-3*N1)), :dash, L"v=v_\nu=-3\mathcal{N}_1")]),
-        ("plots/overdensity.pdf",  L"\log_{10}|\delta|", [(Cosmology.i_δc, log10, :solid, L"\delta=\delta_c"), (Cosmology.i_δb, log10∘abs, :dash, L"\delta=\delta_b")]),
-        ("plots/overdensity2.pdf", L"\log_{10}|\delta|", [(Cosmology.i_Θl(0), Θ0 -> log10(abs(+4*Θ0)), :solid, L"\delta=\delta_\gamma= 4 \Theta_0"), (Cosmology.i_Nl(0), N0 -> log10(abs(+4*N0)), :dash, L"\delta=\delta_\nu= 4\mathcal{N}_0")]),
-        ("plots/Nl0.pdf", L"\mathcal{N}_0", [(Cosmology.i_Nl(0), identity, :solid, "")]),
-        ("plots/Nl1.pdf", L"\mathcal{N}_1", [(Cosmology.i_Nl(1), identity, :solid, "")]),
-        ("plots/Nl2.pdf", L"\mathcal{N}_2", [(Cosmology.i_Nl(2), identity, :solid, "")]),
-        ("plots/ThetaPl0.pdf", L"\Theta^P_0", [(Cosmology.i_ΘPl(0), identity, :solid, "")]),
-        ("plots/ThetaPl1.pdf", L"\Theta^P_1", [(Cosmology.i_ΘPl(1), identity, :solid, "")]),
-        ("plots/ThetaPl2.pdf", L"\Theta^P_2", [(Cosmology.i_ΘPl(2), identity, :solid, "")]),
-        ("plots/Thetal0.pdf", L"\Theta_0", [(Cosmology.i_Θl(0), identity, :solid, "")]),
-        ("plots/Thetal1.pdf", L"\Theta_1", [(Cosmology.i_Θl(1), identity, :solid, "")]),
-        ("plots/Thetal2.pdf", L"\Theta_2", [(Cosmology.i_Θl(2), identity, :solid, "")]),
+    series = [
+        ("plots/potentials.pdf", L"\{\Phi,\Psi\}", [((x,y) -> y[Cosmology.i_Φ](x), :dash, L"\Phi"), ((x,y) -> y[Cosmology.i_Ψ](x), :dot, L"\Psi"), ((x,y) -> y[Cosmology.i_Φ](x) + y[Cosmology.i_Ψ](x), :solid, L"\Phi+\Psi")]),
+
+        ("plots/velocity1.pdf",    L"\log_{10}|v|",      [((x,y) -> log10(abs(   y[Cosmology.i_vc   ](x))), :solid, L"v=v_c"),                          ((x,y) -> log10(abs(   y[Cosmology.i_vb   ](x))), :dash, L"v=v_b"                           )]),
+        ("plots/velocity2.pdf",    L"\log_{10}|v|",      [((x,y) -> log10(abs(-3*y[Cosmology.i_Θl(1)](x))), :solid, L"v=v_\gamma=-3\Theta_1"),          ((x,y) -> log10(abs(-3*y[Cosmology.i_Nl(1)](x))), :dash, L"v=v_\nu=-3\mathcal{N}_1"         )]),
+        ("plots/overdensity1.pdf", L"\log_{10}|\delta|", [((x,y) -> log10(abs(   y[Cosmology.i_δc   ](x))), :solid, L"\delta=\delta_c"),                ((x,y) -> log10(abs(   y[Cosmology.i_δb   ](x))), :dash, L"\delta=\delta_b"                 )]),
+        ("plots/overdensity2.pdf", L"\log_{10}|\delta|", [((x,y) -> log10(abs(+4*y[Cosmology.i_Θl(0)](x))), :solid, L"\delta=\delta_\gamma=4\Theta_0"), ((x,y) -> log10(abs(+4*y[Cosmology.i_Nl(0)](x))), :dash, L"\delta=\delta_\nu=4\mathcal{N}_0")]),
+
+        ("plots/Nl0.pdf", L"\mathcal{N}_0", [((x,y) -> y[Cosmology.i_Nl(0)](x), :solid, "")]),
+        ("plots/Nl1.pdf", L"\mathcal{N}_1", [((x,y) -> y[Cosmology.i_Nl(1)](x), :solid, "")]),
+        ("plots/Nl2.pdf", L"\mathcal{N}_2", [((x,y) -> y[Cosmology.i_Nl(2)](x), :solid, "")]),
+
+        ("plots/ThetaPl0.pdf", L"\Theta^P_0", [((x,y) -> y[Cosmology.i_ΘPl(0)](x), :solid, "")]),
+        ("plots/ThetaPl1.pdf", L"\Theta^P_1", [((x,y) -> y[Cosmology.i_ΘPl(1)](x), :solid, "")]),
+        ("plots/ThetaPl2.pdf", L"\Theta^P_2", [((x,y) -> y[Cosmology.i_ΘPl(2)](x), :solid, "")]),
+
+        ("plots/Thetal0.pdf", L"\Theta_0", [((x,y) -> y[Cosmology.i_Θl(0)](x), :solid, "")]),
+        ("plots/Thetal1.pdf", L"\Theta_1", [((x,y) -> y[Cosmology.i_Θl(1)](x), :solid, "")]),
+        ("plots/Thetal2.pdf", L"\Theta_2", [((x,y) -> y[Cosmology.i_Θl(2)](x), :solid, "")]),
     ]
 
     # pre-compute callable splines once and for all (index and call as y1s[i_k][i_qty](x))
@@ -46,24 +50,25 @@ if true || !isfile("plots/overdensity.pdf") || !isfile("plots/velocity.pdf") || 
     y2s = [Cosmology.perturbations_mode(co, k; tight=true ) for k in ks] # 1D (x) splines for each "exact" k for tight+full system
     y3s = [Cosmology.perturbations_mode(co, k; tight=false) for k in ks] # 1D (x) splines for each "exact" k for       full system
 
-    for (filename, ylabel, iqty_func_linestyle_labels) in settings
+    for (filename, ylabel, func_linestyle_labels) in series
         println("Plotting $filename")
         plot(xlabel=L"x = \log a", ylabel=ylabel, xlims=(-20, 0), xticks=-25:5:5, legend_position=:topleft)
         for (i, k) in enumerate(ks)
             x = extendx(Cosmology.splinex(y3s[i][1]), 7) # plot with extra points between every spline point for more smoothness
-            label = L"k = %$(k*Mpc) / \textrm{Mpc}"
-            for (i_qty, func, linestyle, _) in iqty_func_linestyle_labels
-                plot!(x, func.(y1s[i][i_qty].(x)), alpha=1/8, linewidth=3.0, color=i, linestyle=linestyle, label=nothing)
-                plot!(x, func.(y2s[i][i_qty].(x)), alpha=2/8, linewidth=2.0, color=i, linestyle=linestyle, label=nothing)
-                plot!(x, func.(y3s[i][i_qty].(x)), alpha=8/8, linewidth=1.0, color=i, linestyle=linestyle, label=label)
+            for (func, linestyle, _) in func_linestyle_labels
+                plot!(x, func.(x, Ref(y1s[i])), alpha=0.1, linewidth=3.0, color=i, linestyle=linestyle, label=nothing)
+                plot!(x, func.(x, Ref(y2s[i])), alpha=0.2, linewidth=2.0, color=i, linestyle=linestyle, label=nothing)
+                plot!(x, func.(x, Ref(y3s[i])), alpha=1.0, linewidth=1.0, color=i, linestyle=linestyle, label=nothing)
                 label = nothing # only label each k-value once
             end
+            vline!([-21], color=i, label=L"k = %$(k*Mpc) / \textrm{Mpc}") # label each k-value once
             vline!([time_tight_coupling(co, k)], color=:gray, linestyle=:dash; linewidth=0.5, label=nothing)
         end
+
         # add quantity (if more than one so ambiguous) to legend with a black dummy plot
-        if length(iqty_func_linestyle_labels) > 1
-            for (i_qty, func, linestyle, label) in iqty_func_linestyle_labels
-                vline!([-21], linewidth=1.0, linestyle=linestyle, color=:black, label=label) # dummy outside plot area
+        if length(func_linestyle_labels) > 1
+            for (func, linestyle, label) in func_linestyle_labels
+                vline!([-21], linestyle=linestyle, color=:black, label=label) # dummy outside plot area
             end
         end
         savefig(filename)
